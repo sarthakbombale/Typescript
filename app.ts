e9289fc035d59bf4fcf4b80c1084c2d4c716b898
sarthakbombale <sarthakbomble250@gmail.com>
2026-03-01 23:50:46 +0530

    Infernaces

--- app.ts
+++ app.ts
@@ -1,13 +1,24 @@
 let arr: [string, boolean] = ["hari", false]
 
-enum userRoles{
-    ADMIN="hari",
-    GUEST="OMMI",
-    ROLLNO=12
+enum userRoles {
+    ADMIN = "hari",
+    GUEST = "OMMI",
+    ROLLNO = 12
 }
 
 userRoles.ROLLNO
 
-function aa(){
-     console.log(aa)
+function aa() {
+    console.log(aa)
 }
+
+// type Infernce
+//type annotations
+
+let a: number = 23;
+let b = 'hello';
+
+
+// Interfaces and tpyes alises
+// Defining interfaces
+//Intersection  types
