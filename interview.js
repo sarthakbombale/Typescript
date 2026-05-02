838f906289a896f92ee514dd6271a2d1f7b0b865
sarthakbombale <sarthakbomble250@gmail.com>
2026-05-02 16:58:46 +0530

    practice

--- interview.js
+++ interview.js
@@ -19,4 +19,48 @@ let revNums = [];
 for (let i = nums.length - 1; i >= 0; i--) {
   revNums.push(nums[i]);
 }
-console.log(revNums); p something.");
+console.log(revNums); 
+
+//date 02-07-2024
+
+function reversestring(str){
+    let res = "";
+    for(let i =str.length - 1;i >=0;i--){
+     res += str[i]
+    }
+    return res
+}
+console.log(reversestring("kahtraS"))
+
+let str = "elabmoB";
+let reverse = "";
+for(let i = str.length - 1;i>=0;i --){
+    reverse += str[i];
+}
+console.log(reverse);
+
+let arr = [1,2,3,4,5,6];
+let res=[];
+ res = arr.reverse();
+console.log(res)
+
+for(let i = arr.length - 1;i >=0;i--){
+    res.push(arr[i])
+}
+console.log(res)
+
+//print 1 to 5
+
+for(let i = 1; i <= 5;i++){
+    console.log(i)
+}
+
+for(let i = 1;i <= 5;i++){
+    if(i === 5){
+        console.log(`function for only printing ${i}`);
+        break;
+    }
+}
+
+
+
