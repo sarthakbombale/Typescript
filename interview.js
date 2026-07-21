3357322403361ff182c68bf31f60089da2b549ab
Sarthak Bombale <sarthakbomble250@gmail.com>
2026-07-21 10:16:23 +0530

    Update interview.js

--- interview.js
+++ interview.js
@@ -2064,3 +2064,16 @@ function findMissingNumber(nums) {
     return expectedSum - actualSum;
 }
 console.log(findMissingNumber([1, 2, 4, 5, 6]));
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(11));
+console.log(isPrime(4));
